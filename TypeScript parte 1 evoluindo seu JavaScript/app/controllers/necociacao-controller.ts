edeaeb7ec6c2f960a682b5ee53171101e25dec2b
Arthur Oliveira <arthurdso96@gmail.com>
2022-05-12 16:38:17 -0300

    Criando Controllers

---
+++ TypeScript parte 1 evoluindo seu JavaScript/app/controllers/necociacao-controller.ts
@@ -0,0 +1,18 @@
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona() {
+        console.log(this.inputData);
+        console.log(this.inputData);
+        console.log(this.inputData);
+    }
+    
+}
