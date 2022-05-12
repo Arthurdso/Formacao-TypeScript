ec8e93716d5cbd1c274799c74c225b5498990460
Arthur Oliveira <arthurdso96@gmail.com>
2022-05-12 17:23:23 -0300

    Melhorando Tipagem
    Melhorando Tipagem dos dados

--- TypeScript parte 1 evoluindo seu JavaScript/app/controllers/necociacao-controller.ts
+++ TypeScript parte 1 evoluindo seu JavaScript/app/controllers/necociacao-controller.ts
@@ -1,3 +1,5 @@
+import { Negociacao } from "../models/negociacao.js";
+
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
@@ -9,10 +11,24 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputData);
-        console.log(this.inputData);
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
     }
-    
+
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    } 
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '1';
+        this.inputValor.value = '0.0';
+        this.inputData.focus();
+    }
 }
